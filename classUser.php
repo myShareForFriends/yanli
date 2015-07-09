eecc41661731768d32ed541db11be220b64f0222
王志鹏 <123389141@qq.com>
2015-07-09 23:25:39 +0800

    用户名密码写文件

--- classUser.php
+++ classUser.php
@@ -1,26 +1,57 @@
 <?php
 class classUser {
-	//loing function
-	function login($username,$password){
-		
-		$usertables = array(array("liuyanli","123456"),array("wzp","123456"),array("lyq","qqq"),array("zz","zz"));
-		foreach ($usertables as $user ){
-			if($user[0] == $username && $user[1]== $password){
+	
+	/**
+	 * loing function
+	 * 
+	 * @param string $username        	
+	 * @param string $password        	
+	 * @return boolean 0 1
+	 */
+	function login($username, $password) {
+		$file = file ( "files/user_password" );
+		foreach ( $file as $user ) {
+			$up = explode ( "|", $user );
+			if ($up [0] == $username && trim ( $up [1] ) == $password) {
 				return 1;
 			}
 		}
 		return 0;
 	}
 	
-	//register function
-	function register($username,$password){
-		$usertables = array(array("liuyanli","123456"),array("wzp","123456"),array("lyq","qqq"),array("zz","zz"));
-		foreach ($usertables as $user){
-			if ($user[0] == $username){
-				return "用户已存在";			
+	/**
+	 * exists function
+	 * 
+	 * @param string $username        	
+	 * @param string $password        	
+	 * @return boolean 0 1
+	 */
+	function exists($username) {
+		$file = file ( "files/user_password" );
+		foreach ( $file as $user ) {
+			$up = explode ( "|", $user );
+			if ($up [0] == $username) {
+				return 1;
 			}
 		}
-		return "注册成功";
+		return 0;
+	}
+	
+	/**
+	 * register function
+	 * 
+	 * @param string $username        	
+	 * @param string $password        	
+	 * @return number -1:register false 1:register success
+	 */
+	function register($username, $password) {
+		if ($this->exists ( $username )) {
+			return - 1;
+		} else {
+			$data = "\n" . $username . "|" . $password;
+			file_put_contents ( "files/user_password", $data, FILE_APPEND );
+			return 1;
+		}
 	}
 }
 
